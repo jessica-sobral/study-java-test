9538767e5c97b6e7385d24f4b73df8ec1a42f70e
Jéssica Sobral <jessicasobral5672@gmail.com>
2022-06-24 18:06:27 -0300

    Account class

---
+++ src/main/java/demogradle/Account.java
@@ -0,0 +1,85 @@
+package demogradle;
+
+public class Account {
+    private int id;
+    private double balance;
+    private double limit;
+
+    public Account() {
+
+    }
+
+    public Account(int id, double limit) {
+        this.setId(id);
+        this.setLimit(limit);
+    }
+
+    public String checkBalance() {
+        return "\nAccount " + id + " has balance " + balance;
+    }
+
+    public boolean deposit(double value) {
+        if(value > 0.0) {
+            balance += value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean cashOut(double value) {
+        if(balance >= value) {
+            balance -= value;
+            return true;
+        }
+        else if((balance + limit) >= value) {
+            limit = (balance + limit) - value;
+            balance -= value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean transfer(Account account, double value) {
+        if(balance >= value) {
+            balance -= value;
+            account.balance += value;
+            return true;
+        }
+        else if(balance + limit >= value) {
+            limit = (balance + limit) - value;
+            balance -= value;
+            account.balance += value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        if(id > 0)
+            this.id = id;
+        else
+            throw new IllegalArgumentException("Account code must be greater than zero.");
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public double getLimit() {
+        return limit;
+    }
+
+    public void setLimit(double limit) {
+        if(limit > 0)
+            this.limit = limit;
+        else
+            throw new IllegalArgumentException("Account limit must be greater than zero.");
+    }
+}
--- src/main/java/demogradle/App.java
+++ src/main/java/demogradle/App.java
@@ -13,26 +13,38 @@ public class App {
     public static void main(String[] args) {
         System.out.println(new App().getGretting());
 
-//        Square q1 = new Square(5);
+//        Square s1 = new Square(5);
 //        System.out.println("Square 1\nSide: " + q1.getSide());
 //        System.out.println("Area: " + q1.calculateArea());
 //        System.out.println("Perimeter: " + q1.calculatePerimeter());
 
-//        Square q2 = new Square(10);
+//        Square s2 = new Square(10);
 //        System.out.println("\nSquare 2\nSide: " + q2.getSide());
 //        System.out.println("Area: " + q2.calculateArea());
 //        System.out.println("Perimeter: " + q2.calculatePerimeter());
 
-        Rectangle r1 = new Rectangle(5, 10);
-        System.out.println("\nRectangle 1\nBase: " + r1.getBase());
-        System.out.println("Height: " + r1.getHeight());
-        System.out.println("Area: " + r1.calculateArea());
-        System.out.println("Perimeter: " + r1.calculatePerimeter());
+//        Rectangle r1 = new Rectangle(5, 10);
+//        System.out.println("\nRectangle 1\nBase: " + r1.getBase());
+//        System.out.println("Height: " + r1.getHeight());
+//        System.out.println("Area: " + r1.calculateArea());
+//        System.out.println("Perimeter: " + r1.calculatePerimeter());
 
-        Rectangle r2 = new Rectangle(15, 20);
-        System.out.println("\nRectangle 2\nBase: " + r2.getBase());
-        System.out.println("Height: " + r2.getHeight());
-        System.out.println("Area: " + r2.calculateArea());
-        System.out.println("Perimeter: " + r2.calculatePerimeter());
+//        Rectangle r2 = new Rectangle(15, 20);
+//        System.out.println("\nRectangle 2\nBase: " + r2.getBase());
+//        System.out.println("Height: " + r2.getHeight());
+//        System.out.println("Area: " + r2.calculateArea());
+//        System.out.println("Perimeter: " + r2.calculatePerimeter());
+
+//        Account a1 = new Account(1, 150.00);
+//        a1.deposit(35.50);
+//        System.out.println("\nAccount 1\nId: " + a1.getId());
+//        System.out.println("Balance: " + a1.getBalance());
+//        System.out.println("Limit: " + a1.getLimit());
+
+//        Account a2 = new Account(2, 150.00);
+//        a2.deposit(50.00);
+//        System.out.println("\nAccount 2\nId: " + a2.getId());
+//        System.out.println("Balance: " + a2.getBalance());
+//        System.out.println("Limit: " + a2.getLimit());
     }
 }
