ee28b4f7c5c884a48aaedf036fc8ae92c7e014d3
Jéssica Sobral <jessicasobral5672@gmail.com>
2022-06-25 16:37:49 -0300

    creating test for the calculatePerimeter method of the Rectangle class

--- src/test/java/demogradle/RectangleTest.java
+++ src/test/java/demogradle/RectangleTest.java
@@ -11,4 +11,12 @@ public class RectangleTest {
         double doneResult = r1.calculateArea();
         assertEquals(expectedResult, doneResult, 0);
     }
+
+    @Test
+    public void testCalculatePerimeter() {
+        Rectangle r1 = new Rectangle(5, 10);
+        double expectedResult = 2 * (5 + 10);
+        double doneResult = r1.calculatePerimeter();
+        assertEquals(expectedResult, doneResult, 0);
+    }
 }
