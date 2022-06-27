1b1055275c18816e5673775e08238f010574832a
Jéssica Sobral <jessicasobral5672@gmail.com>
2022-06-27 18:16:20 -0300

    creating test for base equal to zero

--- src/test/java/demogradle/RectangleTest.java
+++ src/test/java/demogradle/RectangleTest.java
@@ -1,6 +1,8 @@
 package demogradle;
 
 import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.Test;
 
 public class RectangleTest {
@@ -28,5 +30,11 @@ public class RectangleTest {
         double expectedResult = 5;
         double doneResult = r1.getBase();
         assertEquals(expectedResult, doneResult, 0);
+    }
+
+    @Test
+    public void testBaseEqualsZero() {
+        Rectangle r1 = new Rectangle(5, 10);
+        assertThrows(IllegalArgumentException.class, () -> { r1.setBase(0); }, "IllegalArgumentException error was expected.");
     }
 }
