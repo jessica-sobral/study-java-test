b092d9029b2bcbc17a42529a016261f26dc72a19
Jéssica Sobral <jessicasobral5672@gmail.com>
2022-06-27 22:16:48 -0300

    creating test for cash out greater than balance plus limit

--- src/test/java/demogradle/AccountTest.java
+++ src/test/java/demogradle/AccountTest.java
@@ -53,6 +53,16 @@ public class AccountTest {
     }
 
     @Test
+    public void testCashOutGreaterThanBalancePlusLimit() {
+        Account a1 = new Account(1, 150);
+        a1.deposit(100);
+        a1.cashOut(300);
+        double expectedResult = 100;
+        double doneResult = a1.getBalance();
+        assertEquals(expectedResult, doneResult, 0);
+    }
+
+    @Test
     public void testIdGreaterThanZero() {
         Account a1 = new Account();
         a1.setId(1);
